5e524b5348f0f60aed318472b0bdc16a7892e4b0
hisamutdinov22 <hisamutdinovaartur18@gmail.com>
2026-09-07 21:41:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const WEBAPP_URL = process.env.WEBAPP_URL;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const FRONTEND_DIR = __dirname, '/frontend');
+const FRONTEND_DIR = __dirname;
 
 async function sendFrontendFile(reply, filename, contentType) {
   try {
